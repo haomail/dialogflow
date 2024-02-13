2090ef79065f5ab36d488266d398a3be6b8850c2
Cantika Aryanti <caryanti96@gmail.com>
2024-02-13 15:21:10 +0700

    Update index.js

--- Hotel Bot/index.js
+++ Hotel Bot/index.js
@@ -24,45 +24,27 @@ app.use(morgan("combined"));
 app.get('/', (req, res)=>{
     res.send("We are live");
 });
-/*
-const moment = require('moment-timezone');
-
-function convertToFormattedDateTime(datePhrase, timePhrase) {
-  const timezone = 'Asia/Jakarta';
-  const currentDate = moment().tz(timezone);
-  const tomorrowDate = moment().add(1, 'day').tz(timezone);
-  const selectedDate = datePhrase.toLowerCase() === 'hari ini' ? currentDate : tomorrowDate;
-  const selectedTime = moment.tz(timePhrase, 'h a', timezone);
-  const combinedDateTime = selectedDate
-  .hour(selectedTime.hour())
-  .minute(selectedTime.minute())
-  .second(0)
-  .millisecond(0);
-  const formattedDateTime = combinedDateTime.format('D MMMM YYYY h A');
-  return formattedDateTime;
-};
-*/ 
 
 app.post('/', express.json(), (req, res)=>{
     const agent = new dfff.WebhookClient({
         request : req,
         response : res
     });
-
+    
+    // 01_Reservation
     function Reservation(agent){
       agent.add("Hai, silahkan lengkapi biodata ini sebelum kamu melanjutkan. Siapa nama anda?");
     }
-    
+
+    // 02_Confirm_Reservation
     function confirmReservation(agent){
       var name = agent.parameters.name.name;
       var email = agent.parameters.email;
       var date = agent.parameters.date;
-      //let dateTime = convertToFormattedDateTime(date);
 
       console.log(name);
       console.log(email);
       console.log(date);
-      //console.log(dateTime);
 
       var ConfirmData = {
         "richContent": [
@@ -92,16 +74,17 @@ app.post('/', express.json(), (req, res)=>{
       return db.collection("reservation").add({
         name: name,
         email: email,
-        date: date,
-        //dateTime: dateTime
+        date: date
         }).then(ref =>
           console.log("Table reservation details added to Database"));
     }
 
+    // 03_Cancel_Confirmation
     function cancelConfirmation(agent){
       agent.add("Terima kasih atas konfirmasinya. Silahkan lanjutkan keperluan anda.");
     }
 
+    // 04_Final_Confirmation
     function finalConfirmation(agent){
       var finalData = {
           "richContent": [
@@ -140,6 +123,7 @@ app.post('/', express.json(), (req, res)=>{
         agent.add(new dfff.Payload(agent.UNSPECIFIED, finalData, {sendAsMessage: true, rawPayload: true}));
     }
 
+    // 05_Metode_Booking
     function MetodeBooking(agent){
       var BookingData = {
         "richContent": [
@@ -187,6 +171,7 @@ app.post('/', express.json(), (req, res)=>{
       agent.add(new dfff.Payload(agent.UNSPECIFIED, BookingData, {sendAsMessage: true, rawPayload: true}));
     }
 
+    // 06_Facilities
     function Facilities(agent){
       var FacilitiesData = {
         "richContent": [
@@ -242,22 +227,27 @@ app.post('/', express.json(), (req, res)=>{
       agent.add(new dfff.Payload(agent.UNSPECIFIED, FacilitiesData, {sendAsMessage: true, rawPayload: true}));
     }
 
+    // 07_CheckIn_Out
     function CheckIn_out(agent){
       agent.add("Check-in dimulai pada pukul [jam] dan check-out harus dilakukan sebelum pukul [jam]. \nSilakan hubungi resepsionis di [nomor handphone] jika Anda membutuhkan penyesuaian.");
     }
 
+    // 08_Cancelled
     function Cancelled(agent){
       agent.add("Kebijakan pembatalan dapat ditemukan di situs web atau konfirmasi pusat pemesanan Anda. \nMohon cek detail pemesanan tiket anda.");
     }
 
+    // 09_Room_Service
     function RoomService(agent){
       agent.add("Kami memiliki restoran di hotel dan juga menyediakan layanan kamar untuk kenyamanan tamu.");
     }
-
+    
+    // 10_Internet
     function Internet(agent){
       agent.add("Wi-Fi gratis dan dapat diakses di seluruh area hotel, termasuk kamar tamu. \nSetelah anda check-in maka anda akan diberikan password untuk wifi tersebut.");
     }
 
+    // 11_Discount
     function Discount(agent){
       agent.add("Untuk mengetahui diskon atau promosi terkini, cek situs web hotel atau hubungi pusat pemesanan. Promo mungkin berlaku pada hari tertentu.");
       var DiscountData = {
@@ -305,38 +295,47 @@ app.post('/', express.json(), (req, res)=>{
       agent.add(new dfff.Payload(agent.UNSPECIFIED, DiscountData, {sendAsMessage: true, rawPayload: true}));
     }
 
+    // 12_Additional_Fee
     function AdditionalFee(agent){
       agent.add("Harga yang tercantum biasanya belum termasuk pajak dan biaya tambahan. \nPastikan untuk memeriksa detail disetiap metode pembayaran sebelum konfirmasi.");
     }
 
+    // 13_Age_Restriction
     function AgeRestriction(agent){
       agent.add("Umumnya, tamu harus berusia minimal 18 tahun untuk check-in. \nPastikan untuk memeriksa persyaratan usia pada saat pemesanan tiket pada metode pembayaran apapun.");
     }
 
+    // 14_Changes
     function Changes(agent){
       agent.add("Silakan hubungi pusat pemesanan atau resepsionis kami dinomor [Nomor Resepsionis] untuk mendiskusikan perubahan yang diperlukan.");
     }
 
+    // 15_Kerusakan
     function Kerusakan(agent){
       agent.add("Ya, hotel memiliki kebijakan terkait kerusakan barang di kamar. \nJika ada kerusakan yang disengaja atau kelalaian yang mengakibatkan kerusakan, hotel berhak mengenakan denda sesuai dengan biaya perbaikan atau penggantian. \nKami selalu mendorong tamu untuk memperlakukan properti hotel dengan baik dan melaporkan setiap kerusakan atau masalah kepada staf kami segera.");
     }
 
+    // 16_LostAndFound
     function LostandFound(agent){
       agent.add("Hubungi resepsionis kami segera. Kami akan berusaha membantu Anda dengan menemukan atau mengganti barang yang tertinggal jika masih dalam hari yang sama. *Dengan syarat tertentu.");
     }
 
+    // 17_Parkir
     function Parkir(agent){
       agent.add("Kami menyediakan fasilitas parkir dan mungkin ada biaya parkir harian. \nPastikan untuk mengetahui kebijakan parkir sebelum kedatangan Anda.");
     }
 
+    // 18_Pet
     function Pet(agent){
       agent.add("Boleh, tapi hanya hewan peliharaan tertentu saja yang diperbolehkan. Silahkan beri tahu kami jika Anda membawa hewan peliharaan.");
     }
 
+    // 19_Privacy
     function Privacy(agent){
       agent.add("Kami menjaga privasi tamu dengan sangat serius. Informasi pribadi Anda dijamin keamanannya sesuai dengan kebijakan privasi kami.");
     }
 
+    // 20_Room_Type
     function RoomType(agent){
       agent.add("Untuk mengetahui jenis kamar dan harga terkini, kunjungi pusat pemesanan.");
       var RoomTypeData = {
@@ -384,10 +383,12 @@ app.post('/', express.json(), (req, res)=>{
       agent.add(new dfff.Payload(agent.UNSPECIFIED, RoomTypeData, {sendAsMessage: true, rawPayload: true}));
     }
 
+    // 21_Security
     function Security(agent){
       agent.add("Kami memiliki sistem keamanan 24 jam dan akses kamar dengan hanya menggunakan kunci.");
     }
 
+    // 22_Strategic
     function Strategic(agent){
       agent.add("Hotel ini berjarak sekitar … dari pusat kota dan berjarak .. ke tempat wisata terkenal ..");
       var StrategicData = {
@@ -409,6 +410,7 @@ app.post('/', express.json(), (req, res)=>{
       agent.add(new dfff.Payload(agent.UNSPECIFIED, StrategicData, {sendAsMessage: true, rawPayload: true}));
     }
 
+    // 23_Transportation
     function Transportation(agent){
       agent.add("Hotel ini berjarak .. ke halte terdekat yang bisa dinaiki transportasi umum … dan berjarak … ke stasiun terdekat …");
       var TransportationData = {
@@ -434,28 +436,28 @@ app.post('/', express.json(), (req, res)=>{
     var intentMap = new Map();
 
     intentMap.set('01_Reservation',Reservation);
-    intentMap.set('confirmReservation',confirmReservation);
-    intentMap.set('cancelConfirmation',cancelConfirmation);
-    intentMap.set('finalConfirmation',finalConfirmation);
-    intentMap.set('MetodeBooking',MetodeBooking);
-    intentMap.set('Facilities',Facilities);
-    intentMap.set('CheckIn_out',CheckIn_out);
-    intentMap.set('Cancelled',Cancelled);
-    intentMap.set('Room Service',RoomService);
-    intentMap.set('Internet',Internet);
-    intentMap.set('Discount',Discount);
-    intentMap.set('Additional Fee',AdditionalFee);
-    intentMap.set('Age Restriction',AgeRestriction);
-    intentMap.set('Changes',Changes);
-    intentMap.set('Kerusakan',Kerusakan);
-    intentMap.set('Lost and Found',LostandFound);
-    intentMap.set('Parkir',Parkir);
-    intentMap.set('Pet',Pet);
-    intentMap.set('Privacy',Privacy);
-    intentMap.set('Room Type',RoomType);
-    intentMap.set('Security',Security);
-    intentMap.set('Strategic',Strategic);
-    intentMap.set('Transportation',Transportation);
+    intentMap.set('02_Confirm_Reservation',confirmReservation);
+    intentMap.set('03_Cancel_Confirmation',cancelConfirmation);
+    intentMap.set('04_Final_Confirmation',finalConfirmation);
+    intentMap.set('05_Metode_Booking',MetodeBooking);
+    intentMap.set('06_Facilities',Facilities);
+    intentMap.set('07_CheckIn_Out',CheckIn_out);
+    intentMap.set('08_Cancelled',Cancelled);
+    intentMap.set('09_Room_Service',RoomService);
+    intentMap.set('10_Internet',Internet);
+    intentMap.set('11_Discount',Discount);
+    intentMap.set('12_Additional_Fee',AdditionalFee);
+    intentMap.set('13_Age_Restriction',AgeRestriction);
+    intentMap.set('14_Changes',Changes);
+    intentMap.set('15_Kerusakan',Kerusakan);
+    intentMap.set('16_LostAndFound',LostandFound);
+    intentMap.set('17_Parkir',Parkir);
+    intentMap.set('18_Pet',Pet);
+    intentMap.set('19_Privacy',Privacy);
+    intentMap.set('20_Room_Type',RoomType);
+    intentMap.set('21_Security',Security);
+    intentMap.set('22_Strategic',Strategic);
+    intentMap.set('23_Transportation',Transportation);
 
     agent.handleRequest(intentMap);
 });
